35d482ddcb0050bea31d6a37aadff71d3dc065a9
Qiuhan Zhou <qiuhanzhou@gmail.com>
2022-10-18 13:54:39 -0400

    change mongoose localhost to 127.0.0.1

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const cardRouter = require('./routes/cards')
 
 const app = express()
 
-mongoose.connect('mongodb://localhost:27017/aroundb', {
+mongoose.connect('mongodb://127.0.0.1:27017/aroundb', {
   useNewUrlParser: true,
 })
 
