29448ee76aa99472ad937a60c5cf2d15fdaaa8c9
Qiuhan Zhou <qiuhanzhou@gmail.com>
2022-10-20 09:27:12 -0400

    fix cors error

--- backend/app.js
+++ backend/app.js
@@ -21,49 +21,25 @@ app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use(routes)
-
-// continue on to use router that does not match public folder thru '/'
-
 // enable cross domain visits from allowed origins
 app.use((req, res, next) => {
-  const allowedCors = [
+  res.header(
+    'Access-Control-Allow-Origin',
     'https://loveali.students.nomoredomainssbs.ru',
-    'http://localhost:3000',
-  ]
-
-  const { origin } = req.headers // saving the request source to the 'origin' variable
-  // checking that the source of the request is mentioned in the list of allowed ones
-  if (allowedCors.includes(origin)) {
-    // setting a header that allows the browser to make requests from this source
-    res.header('Access-Control-Allow-Origin', origin)
-  }
-
-  const { method } = req // Saving the request type (HTTP method) to the corresponding variable
-
-  // Default value for Access-Control-Allow-Methods header (all request types are allowed)
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE'
-
-  // If this is a preliminary request, add the required headers
-  if (method === 'OPTIONS') {
-    // allowing cross-domain requests of any type (default)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS)
-  }
-
-  const requestHeaders = req.headers['access-control-request-headers']
-  if (method === 'OPTIONS') {
-    // allowing cross-domain requests with these headers
-    res.header('Access-Control-Allow-Headers', requestHeaders)
-    // finish processing the request and return the result to the client
-    return res.end()
-  }
-
+  )
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  )
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
   next()
 })
 
 // include these before other routes
 app.use(cors())
 app.options('*', cors()) // enable requests for all routes
+
+app.use(routes)
 
 // REMEMBER: REMOVE AFTER PASS REVIEW
 app.get('/crash-test', () => {
